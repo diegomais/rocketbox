7485d1a07f92d2c474b92d7f8d3468873ae97af3
Diego Mais <diegomais@live.com>
2019-04-09 22:53:33 -0300

    Feat: Provide show in BoxController

--- packages/api/src/controllers/BoxController.js
+++ packages/api/src/controllers/BoxController.js
@@ -6,6 +6,15 @@ class BoxController {
 
     return res.json(box);
   }
+
+  async show(req, res) {
+    const box = await Box.findById(req.params.id).populate({
+      path: 'files',
+      options: { sort: { createdAt: -1 } }
+    });
+
+    return res.json(box);
+  }
 }
 
 module.exports = new BoxController();
--- packages/api/src/routes.js
+++ packages/api/src/routes.js
@@ -9,6 +9,8 @@ const FileController = require('./controllers/FileController');
 
 routes.post('/boxes', BoxController.store);
 
+routes.get('/boxes/:id', BoxController.show);
+
 routes.post(
   '/boxes/:id/files',
   multer(multerConfig).single('file'),
