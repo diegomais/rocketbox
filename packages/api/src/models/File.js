678bb980b44ec485e850d663f26626ab241de40b
Diego Mais <diegomais@live.com>
2019-04-11 21:49:42 -0300

    Feat: Provide Config Vars

--- packages/api/src/models/File.js
+++ packages/api/src/models/File.js
@@ -18,7 +18,9 @@ const File = new mongoose.Schema(
 );
 
 File.virtual('url').get(function () {
-  return `http://localhost:3333/files/${encodeURIComponent(this.path)}`;
+  const url = process.env.URL || 'http://localhost:3333';
+
+  return `${url}/files/${encodeURIComponent(this.path)}`;
 });
 
 module.exports = mongoose.model('File', File);
--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const socketIO = require('socket.io');
 const cors = require('cors');
 
 const app = express();
@@ -9,12 +8,12 @@ const app = express();
 app.use(cors());
 
 const server = require('http').Server(app);
-const io = socketIO(server);
+const io = require('socket.io')(server);
 
 io.on('connection', socket => {
   socket.on('connectRoom', box => {
     socket.join(box);
-  });
+  })
 });
 
 mongoose.connect('mongodb+srv://rocket_6:4HvM61X3CSGCTrep@rocketbox-4tvjd.mongodb.net/omnistack?retryWrites=true', {
@@ -23,14 +22,14 @@ mongoose.connect('mongodb+srv://rocket_6:4HvM61X3CSGCTrep@rocketbox-4tvjd.mongod
 
 app.use((req, res, next) => {
   req.io = io;
-
-  return next;
+  return next();
 });
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
