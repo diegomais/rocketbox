c24146cf4a8062484f556690cff270f2dc389c7c
Diego Mais <diegomais@live.com>
2019-04-14 16:10:45 -0300

    Feat: Provide UI in Main page

--- packages/mobile/src/pages/Main/index.js
+++ packages/mobile/src/pages/Main/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-// import { Container } from './styles';
+import styles from './styles';
+import logo from '../../assets/logo.png';
 
 export default class Main extends Component {
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+        <TextInput
+          style={styles.input}
+          placeholder='New Box Name'
+          placeholderTextColor='#999'
+          autoCapitalize='none'
+          autoCorrect={false}
+          underlineColorAndroid='transparent'
+        />
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.buttonText}>Create New Box</Text>
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
