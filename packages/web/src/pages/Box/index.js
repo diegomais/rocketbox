c83cea4d65e97cb9c9a421ebc6809f0e1d2b8df8
Diego Mais <diegomais@live.com>
2019-04-14 12:38:49 -0300

    Feat: Provide upload files in Box component

--- packages/web/src/pages/Box/index.js
+++ packages/web/src/pages/Box/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone';
 import { distanceInWords } from 'date-fns';
 import { MdInsertDriveFile } from 'react-icons/md';
 import api from '../../services/api';
@@ -14,7 +15,16 @@ export default class Box extends Component {
     const box = this.props.match.params.id;
     const response = await api.get(`boxes/${box}`);
     this.setState({ box: response.data });
-  }
+  };
+
+  handleUpload = files => {
+    files.forEach(file => {
+      const data = new FormData();
+      const box = this.props.match.params.id;
+      data.append('file', file);
+      api.post(`boxes/${box}/files`, data);
+    });
+  };
 
   render() {
     return (
@@ -23,16 +33,24 @@ export default class Box extends Component {
           <img src={logo} alt='rocketbox' />
           <h1>{this.state.box.title}</h1>
         </header>
+        <Dropzone onDropAccepted={this.handleUpload}>
+          {({ getRootProps, getInputProps }) => (
+            <div className='upload' {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drop files or click here to upload</p>
+            </div>
+          )}
+        </Dropzone>
         <ul>
-          { this.state.box.files && this.state.box.files.map(file =>
-            <li key={file.id}>
+          {this.state.box.files && this.state.box.files.map(file =>
+            <li key={file._id}>
               <a className='fileInfo' href={file.url} target='_blank' rel='noopener noreferrer'>
                 <MdInsertDriveFile size={24} color='#a5cfff' />
                 <strong>{file.title}</strong>
               </a>
               <span>Updated {distanceInWords(file.createdAt, new Date())} ago</span>
             </li>
-            )}
+          )}
         </ul>
       </div>
     )
