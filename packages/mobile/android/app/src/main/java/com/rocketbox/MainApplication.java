052c7dd442a0825a12651865de79018672ac6d43
Diego Mais <diegomais@live.com>
2019-04-14 16:41:46 -0300

    Feat: Provide functions in Main component

--- packages/mobile/android/app/src/main/java/com/rocketbox/MainApplication.java
+++ packages/mobile/android/app/src/main/java/com/rocketbox/MainApplication.java
@@ -3,6 +3,11 @@ package com.rocketbox;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.vinzscam.reactnativefileviewer.RNFileViewerPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.rnfs.RNFSPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFileViewerPackage(),
+            new ImagePickerPackage(),
+            new RNFSPackage(),
+            new VectorIconsPackage(),
+            new AsyncStoragePackage(),
             new RNGestureHandlerPackage()
       );
     }
--- packages/mobile/src/pages/Main/index.js
+++ packages/mobile/src/pages/Main/index.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
-
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../../services/api';
 
 import styles from './styles';
 import logo from '../../assets/logo.png';
 
 export default class Main extends Component {
+  state = {
+    newBox: ''
+  };
+
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem('@RocketBox:box');
+
+    if (box) {
+      this.props.navigation.navigate('Box');
+    }
+  };
+
+  handleCreateBox = async () => {
+    const response = await api.post('boxes', {
+      title: this.state.newBox,
+    });
+
+    await AsyncStorage.setItem('@RocketBox:box', response.data._id);
+
+    this.props.navigation.navigate('Box');
+  };
+
   render() {
     return (
       <View style={styles.container}>
@@ -17,8 +41,10 @@ export default class Main extends Component {
           autoCapitalize='none'
           autoCorrect={false}
           underlineColorAndroid='transparent'
+          value={this.state.newBox}
+          onChangeText={text => this.setState({ newBox: text })}
         />
-        <TouchableOpacity onPress={() => {}} style={styles.button}>
+        <TouchableOpacity onPress={this.handleCreateBox} style={styles.button}>
           <Text style={styles.buttonText}>Create New Box</Text>
         </TouchableOpacity>
       </View>
