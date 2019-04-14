7daab30a7c59b37b22f827ff2c434df4b8903249
Diego Mais <diegomais@live.com>
2019-04-14 15:44:51 -0300

    Feat: Provide routes to Main and Box pages

---
+++ packages/mobile/src/pages/Box/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class Box extends Component {
+  render() {
+    return <View />;
+  }
+}
