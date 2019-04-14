563ee964a125110bc5e49f9e52289b310499bd96
Diego Mais <diegomais@live.com>
2019-04-14 12:00:42 -0300

    Feat: Provide UI in Box component

--- packages/web/src/pages/Box/index.js
+++ packages/web/src/pages/Box/index.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
+import { MdInsertDriveFile } from 'react-icons/md';
+
+import logo from '../../assets/logo.svg'
 import './styles.css';
 
 export default class Box extends Component {
   render() {
-    return <div />;
+    return (
+      <div id="box-container">
+        <header>
+          <img src={logo} alt='rocketbox' />
+          <h1>Rocketseat-Box</h1>
+        </header>
+        <ul>
+          <li>
+            <a className='fileInfo' href='/file'>
+              <MdInsertDriveFile size={24} color='#a5cfff' />
+              <strong>Desafio.pdf</strong>
+            </a>
+            <span>Há 3 minutos atrás</span>
+          </li>
+          <li>
+            <a className='fileInfo' href='/file'>
+              <MdInsertDriveFile size={24} color='#a5cfff' />
+              <strong>teste.pdf</strong>
+            </a>
+            <span>Há 20 minutos atrás</span>
+          </li>
+        </ul>
+      </div>
+    )
   }
 }
