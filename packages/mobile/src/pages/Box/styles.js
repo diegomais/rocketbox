74d2103a9510aa4a41ab9aa57e73bd71a3a1dd1c
Diego Mais <diegomais@live.com>
2019-04-14 19:11:47 -0300

    Feat: Provide UI and functions in Box component

--- packages/mobile/src/pages/Box/styles.js
+++ packages/mobile/src/pages/Box/styles.js
@@ -1,22 +1,67 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from "react-native";
+import {
+  getBottomSpace,
+  getStatusBarHeight
+} from "react-native-iphone-x-helper";
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingHorizontal: 20,
+    paddingTop: Platform.OS === "ios" ? getStatusBarHeight() : 0,
+    flex: 1
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  boxTitle: {
+    marginTop: 50,
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+
+  list: {
+    marginTop: 30
   },
+
+  file: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 20
+  },
+
+  separator: {
+    height: 1,
+    backgroundColor: "#EEE"
+  },
+
+  fileInfo: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+
+  fileTitle: {
+    fontSize: 16,
+    color: "#333",
+    marginLeft: 10
+  },
+
+  fileDate: {
+    fontSize: 14,
+    color: "#666"
+  },
+
+  fab: {
+    position: "absolute",
+    right: 30,
+    bottom: 30 + getBottomSpace(),
+    width: 60,
+    height: 60,
+    backgroundColor: "#7159c1",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default styles;
