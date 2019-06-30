44baa37543c5d240cd4e970948c93fc30d99357f
Diego Mais <diegomais@live.com>
2019-06-29 23:24:13 -0300

    Starting application
    - These chore create an application from scratch using Express.
    - In this application was configured the following tools:
      * Sucrase + Nodemon;
      * ESLint + Prettier + EditorConfig;
      * Sequelize (Using PostgreSQL);
    
    On branch starting-application
    Changes to be committed:
    	new file:   .editorconfig
    	new file:   .eslintrc.js
    	new file:   .gitignore
    	new file:   .prettierrc
    	new file:   .sequelizerc
    	new file:   nodemon.json
    	new file:   package.json
    	new file:   src/app.js
    	new file:   src/app/controllers/SessionController.js
    	new file:   src/app/controllers/UserController.js
    	new file:   src/app/middlewares/auth.js
    	new file:   src/app/models/User.js
    	new file:   src/config/auth.js
    	new file:   src/config/database.js
    	new file:   src/database/index.js
    	new file:   src/database/migrations/20190627021104-create-users.js
    	new file:   src/routes.js
    	new file:   src/server.js
    	new file:   yarn.lock

---
+++ packages/api/src/app/controllers/SessionController.js
@@ -0,0 +1,51 @@
+import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    // Create a object schema validator and object parser using Yup
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+    });
+
+    // Check validity
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Invalid parameters.' });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: `Sorry, we don't recognize this email.` });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res
+        .status(401)
+        .json({ error: 'Invalid password. Please try again.' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: { id, name, email },
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
---
+++ packages/api/src/server.js
@@ -0,0 +1,8 @@
+import app from './app';
+
+/** Create a constant reference to port declared on environment variables
+ * or set the port of remote server to 3333.  */
+const PORT = process.env.PORT || 3333;
+
+// Bind and listen for connections on the specified port.
+app.listen(PORT);
