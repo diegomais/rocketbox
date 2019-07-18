69cb7a3fa577ce3223fe347a530de37bf9e076bd
Diego Mais <diegomais@live.com>
2019-07-18 06:30:20 -0300

    Add files table in database
    - This table references the file uploaded by the user.
    
    CLI commands:
    1. yarn sequelize migration:create --name=create-files
    2. yarn sequelize db:migrate
    
    Changes to be committed:
    	new file:   src/app/controllers/FileController.js
    	new file:   src/app/models/File.js
    	modified:   src/database/index.js
    	new file:   src/database/migrations/20190718091456-create-files.js
    	modified:   src/routes.js

--- packages/api/src/routes.js
+++ packages/api/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -19,8 +20,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json(req.file);
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
