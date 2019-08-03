5e71910046214a79da2be244ca8710ef8e37bf1c
Diego Mais <diegomais@live.com>
2019-08-03 18:49:44 -0300

    Add private routes
    - Some components need to be locked down if a user is not logged in.
    - If a user is logged in, the required route will be rendered. But if a user is not logged in, they will be redirected to home.
    
    On branch master
    
    Changes to be committed:
    	modified:   package.json
    	new file:   src/routes/Route.js
    	modified:   src/routes/index.js

--- packages/web/src/routes/index.js
+++ packages/web/src/routes/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -16,11 +18,11 @@ export default function services() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/profile" component={Profile} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/create" component={CreateMeetup} />
-      <Route path="/meetup" component={Meetup} />
-      <Route path="/edit" component={EditMeetup} />
+      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/create" component={CreateMeetup} isPrivate />
+      <Route path="/meetup" component={Meetup} isPrivate />
+      <Route path="/edit" component={EditMeetup} isPrivate />
     </Switch>
   );
 }
