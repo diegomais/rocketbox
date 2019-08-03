7fdec0f8971fbd10d88389c23923bf03ae77de84
Diego Mais <diegomais@live.com>
2019-08-03 18:36:13 -0300

    Add Reactotron
    - Reactotron is a desktop app for inspecting React JS projects.
    
    On branch master
    
    Changes to be committed:
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/config/Reactotron.js
    	modified:   yarn.lock

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -4,6 +4,8 @@ import { Router } from 'react-router-dom';
 import Routes from './routes';
 import history from './services/history';
 
+import './config/Reactotron';
+
 function App() {
   return (
     <Router history={history}>
