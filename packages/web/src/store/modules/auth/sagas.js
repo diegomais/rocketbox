997f56a6271b5e9e8023b9dcbcb251800da2f6ef
Diego Mais <diegomais@live.com>
2019-08-03 21:10:45 -0300

    Add sign in feature
    - These improvements provide user to sign in and store authentication and user data into state container.
    
    On branch master
    
    Changes to be committed:
    	modified:   package.json
    	modified:   src/pages/SignIn/index.js
    	modified:   src/routes/Route.js
    	new file:   src/services/api.js
    	modified:   src/store/modules/auth/actions.js
    	modified:   src/store/modules/auth/reducer.js
    	modified:   src/store/modules/auth/sagas.js
    	modified:   src/store/modules/rootReducer.js
    	modified:   src/store/modules/rootSaga.js
    	new file:   src/store/modules/user/actions.js
    	new file:   src/store/modules/user/reducer.js
    	new file:   src/store/modules/user/sagas.js
    	modified:   yarn.lock

--- packages/web/src/store/modules/auth/sagas.js
+++ packages/web/src/store/modules/auth/sagas.js
@@ -1,3 +1,22 @@
-import { all } from 'redux-saga/effects';
+import { call, put, all, takeLatest } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', { email, password });
+
+  console.tron.log(response);
+
+  const { token, user } = response.data;
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
--- packages/web/src/store/modules/user/reducer.js
+++ packages/web/src/store/modules/user/reducer.js
@@ -1,17 +1,14 @@
 import produce from 'immer';
 
 const initialState = {
-  token: null,
-  signed: false,
-  loading: false,
+  profile: null,
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case '@auth/SIGN_IN_SUCCESS':
       return produce(state, draft => {
-        draft.token = payload.token;
-        draft.signed = true;
+        draft.profile = payload.user;
       });
     default:
       return state;
