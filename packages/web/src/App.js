4e469f158e2df47942886e4e18184537daa59300
Diego Mais <diegomais@live.com>
2019-08-03 21:18:34 -0300

    Add persist and rehydrate redux store data
    - Persist and rehydrate redux store data.
    
    On branch master
    
    Changes to be committed:
    	modified:   package.json
    	modified:   src/App.js
    	modified:   src/routes/Route.js
    	modified:   src/store/index.js
    	new file:   src/store/persistReducers.js
    	modified:   yarn.lock

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './config/Reactotron';
 
-import store from './store';
+import { store, persistor } from './store';
 import history from './services/history';
 
 import Routes from './routes';
@@ -13,10 +14,12 @@ import GlobalStyles from './styles/global';
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyles />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyles />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
