867fb95e2c6957327ba485786289271d38c80d03
Diego Mais <diegomais@live.com>
2019-08-04 10:45:45 -0300

    Add authorization header to requests
    - This improvement send authorization header to requests when a user is authenticated.
    
    On branch master
    
    Changes to be committed:
    	modified:   src/pages/Dashboard/index.js
    	modified:   src/store/modules/auth/sagas.js

--- packages/web/src/pages/Dashboard/index.js
+++ packages/web/src/pages/Dashboard/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import api from '~/services/api';
+
 // import { Container } from './styles';
 
 export default function Dashboard() {
+  api.get('hosts');
+
   return <div>Dashboard</div>;
 }
--- packages/web/src/store/modules/auth/sagas.js
+++ packages/web/src/store/modules/auth/sagas.js
@@ -14,6 +14,8 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
@@ -38,7 +40,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
