38d3ee6c80cf5f455a970cd0999e2e713ba516f9
Diego Mais <diegomais@live.com>
2019-08-27 23:52:05 -0300

    Fix background height
    - The background was not covering the entire page.
    - Changing the height property to min-height.
    
    On branch master
    Changes to be committed:
    	modified:   src/pages/_layouts/auth/styles.js
    	modified:   src/pages/_layouts/default/styles.js

--- packages/web/src/pages/_layouts/auth/styles.js
+++ packages/web/src/pages/_layouts/auth/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { darken } from 'polished';
 
 export const Wrapper = styled.div`
-  height: 100%;
+  min-height: 100%;
   background: linear-gradient(180deg, #22202c, #402845);
   display: flex;
   justify-content: center;
