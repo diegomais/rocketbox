e86f4cb00d9786c1f32c522c7de19aa388d06e75
Diego Mais <diegomais@live.com>
2019-08-28 14:05:33 -0300

    Add create meetup page
    - User must be able to create meetups.
    
    On branch master
    Changes to be committed:
    	modified:   package.json
    	new file:   src/components/BannerInput/index.js
    	new file:   src/components/BannerInput/styles.js
    	new file:   src/components/DatePicker/index.js
    	new file:   src/components/DatePicker/styles.js
    	modified:   src/pages/CreateMeetup/index.js
    	new file:   src/pages/CreateMeetup/styles.js
    	modified:   yarn.lock

---
+++ packages/web/src/components/BannerInput/index.js
@@ -0,0 +1,64 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { useField } from '@rocketseat/unform';
+import { MdAddAPhoto } from 'react-icons/md';
+
+import api from '~/services/api';
+
+import { Container } from './styles';
+
+export default function BannerInput() {
+  const { defaultValue, registerField } = useField('banner');
+
+  const [file, setFile] = useState(defaultValue && defaultValue.id);
+  const [preview, setPreview] = useState(defaultValue && defaultValue.url);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    if (ref.current) {
+      registerField({
+        name: 'file_id',
+        ref: ref.current,
+        path: 'dataset.file',
+      });
+    }
+  }, [ref, registerField]);
+
+  async function handleChange(e) {
+    const data = new FormData();
+
+    data.append('file', e.target.files[0]);
+
+    const response = await api.post('files', data);
+
+    const { id, url } = response.data;
+
+    setFile(id);
+    setPreview(url);
+  }
+
+  return (
+    <Container>
+      <label htmlFor="banner">
+        {preview ? (
+          <img src={preview} alt="Banner" />
+        ) : (
+          <div>
+            <MdAddAPhoto size={54} />
+            Upload a image
+          </div>
+        )}
+
+        <input
+          type="file"
+          id="banner"
+          accept="image/*"
+          data-file={file}
+          onChange={handleChange}
+          ref={ref}
+        />
+      </label>
+      {/* {error && <span>{error}</span>} */}
+    </Container>
+  );
+}
--- packages/web/src/pages/CreateMeetup/index.js
+++ packages/web/src/pages/CreateMeetup/index.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+import { MdAddCircleOutline } from 'react-icons/md';
 
-// import { Container } from './styles';
+import api from '~/services/api';
+import history from '~/services/history';
+
+import BannerInput from '~/components/BannerInput';
+import DatePicker from '~/components/DatePicker';
+import Button from '~/components/Button';
+import Spinner from '~/components/Spinner';
+
+import { Container } from './styles';
+
+const schema = Yup.object().shape({
+  title: Yup.string().required('Title is required.'),
+  description: Yup.string().required('Description is required.'),
+  date: Yup.date().required('Date is required.'),
+  location: Yup.string().required('Location is required.'),
+  file_id: Yup.string().required('Image is required.'),
+});
 
 export default function CreateMeetup() {
-  return <div>CreateMeetup</div>;
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(data) {
+    try {
+      setLoading(true);
+
+      const response = await api.post('meetups', data);
+
+      toast.success('Meetup created successfully!');
+
+      setLoading(false);
+
+      history.push(`/meetup/${response.data.id}`);
+    } catch (err) {
+      setLoading(false);
+      toast.error('There was an error creating your meetup. Try again later.');
+    }
+  }
+
+  return !loading ? (
+    <Container>
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <BannerInput name="file_id" />
+
+        <Input name="title" placeholder="Meetup title" />
+        <Input multiline name="description" placeholder="Description" />
+        <DatePicker name="date" placeholder="Date of meetup" />
+        <Input name="location" placeholder="Location of meetup" />
+        <Button type="submit">
+          <MdAddCircleOutline size={20} />
+          Save meetup
+        </Button>
+      </Form>
+    </Container>
+  ) : (
+    <Spinner />
+  );
 }
