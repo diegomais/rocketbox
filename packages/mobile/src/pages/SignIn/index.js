0f4191fde2a598b1ce520d5ace7f74987f7998ac
Diego Mais <diegomais@live.com>
2019-08-29 18:10:39 -0300

    Add sign in page UI
    - This page provides user to sign into app.
    
    On branch master
    Changes to be committed:
    	new file:   src/assets/logo.png
    	new file:   src/assets/logo@2x.png
    	new file:   src/assets/logo@3x.png
    	modified:   src/pages/SignIn/index.js
    	new file:   src/pages/SignIn/styles.js

--- packages/mobile/src/pages/SignIn/index.js
+++ packages/mobile/src/pages/SignIn/index.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import { Text } from 'react-native';
+import PropTypes from 'prop-types';
 
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
-// import { Container } from './styles';
+import {
+  Container,
+  Image,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+  Strong,
+} from './styles';
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            placeholder="Email Address"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <FormInput
+            icon="lock-outline"
+            placeholder="Password"
+            secureTextEntry
+          />
+          <SubmitButton onPress={() => {}}>Log in</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>
+            Don’t have an account? <Strong>Sign up</Strong>
+          </SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
+
+SignIn.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
