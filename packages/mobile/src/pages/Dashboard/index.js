026e1a2efa12a2f85f5d3d5d4c9b4e58e47df500
Diego Mais <diegomais@live.com>
2019-08-30 14:56:27 -0300

    Add navigation between days on dashboard
    - This improvement provides user to navigate between days on dashboard.
    
    On branch master
    Changes to be committed:
    	modified:   package.json
    	modified:   src/pages/Dashboard/index.js
    	new file:   src/pages/Dashboard/styles.js
    	modified:   yarn.lock

--- packages/mobile/src/pages/Dashboard/index.js
+++ packages/mobile/src/pages/Dashboard/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import { TouchableWithoutFeedback } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import PropTypes from 'prop-types';
 
 import Background from '~/components/Background';
-// import { Container } from './styles';
+import { Container, Nav, Title } from './styles';
 
 export default function Dashboard() {
-  return <Background />;
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(() => format(date, 'EEE, MMM d'), [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Nav>
+          <TouchableWithoutFeedback onPress={handlePrevDay}>
+            <Icon name="chevron-left" size={30} color="#fff" />
+          </TouchableWithoutFeedback>
+          <Title>{dateFormatted}</Title>
+          <TouchableWithoutFeedback onPress={handleNextDay}>
+            <Icon name="chevron-right" size={30} color="#fff" />
+          </TouchableWithoutFeedback>
+        </Nav>
+      </Container>
+    </Background>
+  );
 }
 
 function TabBarIcon({ tintColor }) {
---
+++ packages/mobile/src/pages/Dashboard/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  margin: 0 20px;
+`;
+
+export const Nav = styled.View`
+  flex-direction: row;
+  align-self: center;
+  align-items: center;
+  margin: 30px 0;
+`;
+
+export const Title = styled.Text`
+  margin: 0 15px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+`;
