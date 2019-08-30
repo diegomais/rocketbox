a17055876c3d71c6f5f715ec4e8e320f45ce0c38
Diego Mais <diegomais@live.com>
2019-08-30 00:37:02 -0300

    Add update profile page
    - This page provides user to update their personal data.
    
    On branch master
    Changes to be committed:
    	modified:   src/pages/Profile/index.js
    	new file:   src/pages/Profile/styles.js

--- packages/mobile/src/pages/Profile/index.js
+++ packages/mobile/src/pages/Profile/index.js
@@ -1,12 +1,118 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import PropTypes from 'prop-types';
 
+import { updateProfileRequest } from '~/store/modules/user/actions';
 import Background from '~/components/Background';
-// import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Form,
+  FormInput,
+  SubmitButton,
+  Separator,
+} from './styles';
 
 export default function Profile() {
-  return <Background />;
+  const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
+
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const emailRef = useRef();
+  const oldPasswordRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  // Clean text input after profile is updated
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile]);
+
+  function handleSubmit() {
+    dispatch(
+      updateProfileRequest({
+        name,
+        email,
+        oldPassword,
+        password,
+        confirmPassword,
+      })
+    );
+  }
+
+  return (
+    <Background>
+      <Container>
+        <Title>My profile</Title>
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            placeholder="Full name"
+            value={name}
+            onChangeText={setName}
+            autoCorrect={false}
+            autoCapitalize="words"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+          />
+          <FormInput
+            icon="mail-outline"
+            placeholder="Email address"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => oldPasswordRef.current.focus()}
+          />
+          <Separator />
+          <FormInput
+            icon="lock-outline"
+            placeholder="Current password"
+            value={oldPassword}
+            onChangeText={setOldPassword}
+            secureTextEntry
+            ref={oldPasswordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            placeholder="New password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            ref={passwordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => confirmPasswordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            placeholder="Confirm new password"
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+            secureTextEntry
+            ref={confirmPasswordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+          <SubmitButton onPress={handleSubmit}>Update profile</SubmitButton>
+        </Form>
+      </Container>
+    </Background>
+  );
 }
 
 function TabBarIcon({ tintColor }) {
