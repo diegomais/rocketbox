5ab5d53741ecd90e3abf03e123617b2baca9d72a
Diego Mais <diegomais.dev@gmail.com>
2021-02-03 00:53:04 -0300

    🔨 add local development environment using Docker

--- packages/api/src/models/File.js
+++ packages/api/src/models/File.js
@@ -18,7 +18,7 @@ const File = new mongoose.Schema(
 );
 
 File.virtual('url').get(function () {
-  const url = process.env.URL || 'http://localhost:3333';
+  const url = process.env.API_URL || 'http://localhost:3333';
 
   return `${url}/files/${encodeURIComponent(this.path)}`;
 });
--- packages/api/src/server.js
+++ packages/api/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -13,18 +14,19 @@ const io = require('socket.io')(server);
 io.on('connection', socket => {
   socket.on('connectRoom', box => {
     socket.join(box);
-  })
+  });
 });
 
-mongoose.connect('mongodb+srv://rocket_6:4HvM61X3CSGCTrep@rocketbox-4tvjd.mongodb.net/omnistack?retryWrites=true', {
-  useNewUrlParser: true
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    'mongodb://mongodb:27017/rocketbox?retryWrites=true',
+  { useNewUrlParser: true }
+);
 
 app.use((req, res, next) => {
   req.io = io;
   return next();
 });
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
